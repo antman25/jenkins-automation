8b627d80468594b77a634c3ec19b5ebd03997cd3
Anthony Magner <anthony.magner@gmail.com>
2023-02-26 22:47:48 -0700

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -5,7 +5,7 @@
 
 private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
 private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
-private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
+private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-\$)");
 
 String slugify(String input) {
     String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
