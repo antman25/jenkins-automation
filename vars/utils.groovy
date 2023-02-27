00bb0f801c61ba3d9663853f72689dab2e710a12
Anthony Magner <anthony.magner@gmail.com>
2023-02-26 23:10:22 -0700

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,15 +1,7 @@
-/*String slugify(String input) {
-    def result =input.replaceAll(/[^\w\s-]/, '').trim().toLowerCase()
-    return input.replaceAll(/[-\s]+/, '-')
-}*/
-
-
 String slugify(String input) {
     //return input.toLowerCase().replaceAll("[^a-z0-9-]", "");
     def result = input.replaceAll('\\/','-').toLowerCase()
-    println("First Replace: ${result}")
     result = result.replaceAll(/[^\w\s-]/, '')
-    println("Second Replace: ${result}")
     return result.replaceAll(/[-\s]+/, '-')
 }
 
@@ -19,92 +11,11 @@ String isPrimaryBranch(String branchName) {
     return false
 }
 
-Map envVarExists(String key) {
-  return env.getProperty(key) != null
-}
-
-String getLongCommit()
-{
+String getLongCommit() {
     return sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
 }
 
-def getModifiedFiles(commit_id)
-{
-    def cmd = "git diff --name-only ${commit_id}"
-    def output = cmd.execute().text.trim()
-    print("getModifiedFilesStdout: ${output}")
-    def file_list = output.split('\n')
-    print("getModifiedList: ${file_list}")
-
-    return output
-}
-
-boolean toBoolean(def value) {
-  return (value instanceof java.lang.String) ? value.toBoolean() : value
-}
-
-
-
-def default_stages(Closure body)
-{
-  return {
-      stage('Clone Code')
-      {
-          checkout scm
-      }
-
-      stage('Additional Setup')
-      {
-
-      }
-      body()
-
-      stage('Code Scan')
-      {
-
-      }
-
-      stage('Post Cleanup')
-      {
-
-      }
-  }
-}
-
-void dumpConfig(Map config)
-{
-    String output = ""
-    config.each {it ->
-        output += "${it}\n"
-    }
-    println("Config Dump:\n${output}")
-}
-
-Map getConfig(key = null) {
-
-    String branch_name = env.getEnvironment().getOrDefault('BRANCH_NAME', 'main')
-    String branch_name_safe = sanitizeBranchName(branch_name)
-    String delivery_branch = env.getEnvironment().getOrDefault('DELIVERY_BRANCH', 'main')
-    //String chart_path = env.getEnvironment().getOrDefault('CHART_PATH', './helm')
-    String dockerfile_path = env.getEnvironment().getOrDefault('DOCKERFILE_PATH', '.')
-    String agent_pvc_name = env.getEnvironment().getOrDefault('AGENT_PVC_NAME', 'jenkins-agent-pvc')
-    String pipeline_root = env.getEnvironment().getOrDefault('PIPELINE_ROOT', 'pipeline-root')
-    String tenant = "default";
-
-    withFolderProperties {
-        tenant = "${env.TENANT}"
-    }
-  def config = [   branchName : branch_name,
-                                branchNameSafe : branch_name_safe,
-                                branchDelivery: delivery_branch,
-                                isDeliveryBranch: branch_name == delivery_branch,
-                                tenant : tenant,
-                                agentPvcName: agent_pvc_name,
-                                folderPipelineRoot: pipeline_root
-                            ]
-
-
-
-  return key ? config[key] : config
-
+def getModifiedFiles(String commit_id) {
+    def output = sh (returnStdout: true, script: "git diff --name-only ${commit_id}")
+    return output.tokenize('\n')
 }
